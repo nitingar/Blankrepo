bfa904e9f9421e11bad26484cc09a85d41a8199e
nitingar <nitingar@DIN51002890.corp.capgemini.com>
2016-11-04 15:46:10 +0530

    restJson Project

---
+++ RestJson/src/main/java/com/capgemini/RestJson/MyResource.java
@@ -0,0 +1,30 @@
+package com.capgemini.RestJson;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("/xml/myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    
+    public Customer getIt() {
+    	
+    	Customer cust  = new Customer();
+    	
+    	cust.setAge(30);
+        return cust;
+    }
+}
